fe26aa6f907b2186b817d21b8e3bfdac68e6a079
Neil Prabhu <nprabh8@illinois.edu>
2022-06-20 17:10:05 -0500

    Added Splash Screen

--- app/src/main/java/com/example/pilotpal/IntroPage.java
+++ app/src/main/java/com/example/pilotpal/IntroPage.java
@@ -5,13 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.Window;
 import android.view.WindowManager;
 
 public class IntroPage extends AppCompatActivity {
 
-    private float x1, x2, y1, y2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,22 +21,14 @@ public class IntroPage extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_intro_page);
-    }
-    public boolean onTouchEvent(MotionEvent touchevent) {
-        switch (touchevent.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                x1 = touchevent.getX();
-                y1 = touchevent.getY();
-                break;
-            case MotionEvent.ACTION_UP:
-                x2 = touchevent.getX();
-                y2 = touchevent.getY();
-                if (x1 > x2) {
-                    Intent intent = new Intent(IntroPage.this, MainMenu.class);
-                    startActivity(intent);
-                }
-                break;
-        }
-        return false;
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent intent = new Intent(IntroPage.this, MainMenu.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
     }
 }
