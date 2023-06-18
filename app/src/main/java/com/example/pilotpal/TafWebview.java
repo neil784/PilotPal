b9d6e7016c053c4a15717dca338c4bad6e3bdea6
neil784 <nprabh8@illinois.edu>
2023-06-18 14:26:49 -0500

    Updated WX & Airport webviews to wait to display until page loaded

--- app/src/main/java/com/example/pilotpal/TafWebview.java
+++ app/src/main/java/com/example/pilotpal/TafWebview.java
@@ -3,7 +3,9 @@ package com.example.pilotpal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.pm.ActivityInfo;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.webkit.WebView;
@@ -11,6 +13,7 @@ import android.webkit.WebViewClient;
 
 public class TafWebview extends AppCompatActivity {
     WebView webView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,16 +25,24 @@ public class TafWebview extends AppCompatActivity {
         webView = findViewById(R.id.webview);
         Bundle extras = getIntent().getExtras();
         String icao = extras.getString("ICAO");
-        String url = "https://www.checkwx.com/weather/"
-                + icao
-                + "/taf";
+        String url = "https://www.checkwx.com/weather/" + icao + "/taf";
         webView.getSettings().setJavaScriptEnabled(true);
+        webView.setVisibility(View.INVISIBLE); // Hide the WebView initially
+
         webView.setWebViewClient(new WebViewClient() {
             @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                // Called when the WebView starts loading a new page
+                webView.setVisibility(View.INVISIBLE); // Hide the WebView
+            }
+
+            @Override
             public void onPageFinished(WebView view, String url) {
+                // Called when the WebView finishes loading a page
                 view.loadUrl("javascript:(function() { " +
                         "var element = document.getElementsByClassName('max-w-8xl mx-auto p-4 md:px-6 flex flex-col')[0]; " +
                         "document.body.innerHTML = element.outerHTML; })();");
+                webView.setVisibility(View.VISIBLE); // Show the WebView after the page has finished loading
             }
         });
         webView.loadUrl(url);
