33454240da7385daab2aa4f37a3dd80699668fb8
neil784 <nprabh8@illinois.edu>
2023-07-03 22:54:13 -0500

    Fixed IntroPage Activity warnings and imports

--- app/src/main/java/com/example/pilotpal/IntroPage.java
+++ app/src/main/java/com/example/pilotpal/IntroPage.java
@@ -1,35 +1,29 @@
 package com.example.pilotpal;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.MotionEvent;
 import android.view.Window;
 import android.view.WindowManager;
+import java.util.Objects;
 
 public class IntroPage extends AppCompatActivity {
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        getSupportActionBar().hide();
+        Objects.requireNonNull(getSupportActionBar()).hide();
         setContentView(R.layout.activity_intro_page);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(IntroPage.this, MainMenu.class);
-                startActivity(intent);
-                finish();
-                overridePendingTransition(R.anim.slide_in_down, R.anim.slide_out_down);
-            }
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(IntroPage.this, MainMenu.class);
+            startActivity(intent);
+            finish();
+            overridePendingTransition(R.anim.slide_in_down, R.anim.slide_out_down);
         }, 2000);
     }
 }
