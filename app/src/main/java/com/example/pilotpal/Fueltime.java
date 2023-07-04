3336e9d142cdc22562bfa330a6d330a4e259b453
neil784 <nprabh8@illinois.edu>
2023-07-04 16:08:17 -0500

    Fixed Fueltime Activity warnings and imports

--- app/src/main/java/com/example/pilotpal/Fueltime.java
+++ app/src/main/java/com/example/pilotpal/Fueltime.java
@@ -14,7 +14,6 @@ import java.util.Objects;
 
 public class Fueltime extends AppCompatActivity {
     ActivityFueltimeBinding view;
-    private static double fthours = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,7 +51,7 @@ public class Fueltime extends AppCompatActivity {
 
             if (!distanceInput.isEmpty() && !groundSpeedInput.isEmpty() && ! fuelConsumptionInput.isEmpty() && Integer.parseInt(groundSpeedInput) != 0) {
                 calculateAndDisplayFlightTime(Double.parseDouble(view.distanceEditText.getText().toString()), Double.parseDouble(view.groundSpeedEditText.getText().toString()));
-                calculateAndDisplayFuelRequired(hoursToMinutes(fthours), Double.parseDouble(view.fuelConsumptionEditText.getText().toString()));
+                calculateAndDisplayFuelRequired(Double.parseDouble(view.distanceEditText.getText().toString()), Double.parseDouble(view.groundSpeedEditText.getText().toString()), Double.parseDouble(view.fuelConsumptionEditText.getText().toString()));
             } else if (!distanceInput.isEmpty() && !groundSpeedInput.isEmpty() && Integer.parseInt(groundSpeedInput) != 0) {
                 calculateAndDisplayFlightTime(Double.parseDouble(view.distanceEditText.getText().toString()), Double.parseDouble(view.groundSpeedEditText.getText().toString()));
             }
@@ -61,37 +60,29 @@ public class Fueltime extends AppCompatActivity {
         public void afterTextChanged(Editable s) { }
     };
     private void calculateAndDisplayFlightTime(double distance, double groundSpeed) {
-        fthours = distance / groundSpeed;
+        double fthours = distance / groundSpeed;
         String hours = new DecimalFormat("00").format((int) fthours);
-        double ftminutes = hoursToMinutes(fthours - Double.parseDouble(hours));
+        double ftminutes = 60 * (fthours - Double.parseDouble(hours));
         String minutes = new DecimalFormat("00").format((int) ftminutes);
-        double ftseconds = minutesToSeconds(ftminutes - Double.parseDouble(minutes));
+        double ftseconds = 60 * (ftminutes - Double.parseDouble(minutes));
         String seconds = new DecimalFormat("00").format((int) ftseconds);
         view.flightTime.setText(getString(R.string.flight_time_display, hours, minutes, seconds));
     }
-    private void calculateAndDisplayFuelRequired(double flightTime, double fuelConsumption) {
-        double hourConverted = minutesToHours(flightTime);
+    private void calculateAndDisplayFuelRequired(double distance, double groundSpeed, double fuelConsumption) {
+        double flightTime = 60 * (distance / groundSpeed);
+        double hourConverted = flightTime / 60;
         double calculatedFuelRequired = fuelConsumption * hourConverted;
 
         flightTime += 30;
-        double hourConverted30 = minutesToHours(flightTime);
+        double hourConverted30 = flightTime / 60;
         double calculatedFuelRequired30 = fuelConsumption * hourConverted30;
 
         flightTime += 15;
-        double hourConverted45 = minutesToHours(flightTime);
+        double hourConverted45 = flightTime / 60;
         double calculatedFuelRequired45 = fuelConsumption * hourConverted45;
 
         view.fuelRequired.setText(getString(R.string.fuel_display, calculatedFuelRequired));
         view.fuelRequired30.setText(getString(R.string.fuel_display, calculatedFuelRequired30));
         view.fuelRequired45.setText(getString(R.string.fuel_display, calculatedFuelRequired45));
-    }
-    public static double hoursToMinutes(double hours) {
-        return hours * 60;
-    }
-    public static double minutesToSeconds(double minutes) {
-        return minutes * 60;
-    }
-    public static double minutesToHours(double minutes) {
-        return minutes / 60;
     }
 }
