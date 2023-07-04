45c3e7738a5716ddb5a1583ce3cc05133e01b3ba
neil784 <nprabh8@illinois.edu>
2023-07-03 22:35:47 -0500

    Fixed TafWebview Activity warnings and imports

--- app/src/main/java/com/example/pilotpal/TafWebview.java
+++ app/src/main/java/com/example/pilotpal/TafWebview.java
@@ -1,7 +1,6 @@
 package com.example.pilotpal;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.pm.ActivityInfo;
 import android.graphics.Bitmap;
 import android.os.Bundle;
@@ -10,41 +9,43 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import com.example.pilotpal.databinding.ActivityTafWebviewBinding;
+import java.util.Objects;
 
 public class TafWebview extends AppCompatActivity {
-    WebView webView;
-
+    ActivityTafWebviewBinding view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        view = ActivityTafWebviewBinding.inflate(getLayoutInflater());
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        getSupportActionBar().hide();
-        setContentView(R.layout.activity_taf_webview);
-        webView = findViewById(R.id.webview);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+        setContentView(view.getRoot());
+
         Bundle extras = getIntent().getExtras();
         String icao = extras.getString("ICAO");
         String url = "https://www.checkwx.com/weather/" + icao + "/taf";
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.setVisibility(View.INVISIBLE); // Hide the WebView initially
+        view.webView.getSettings().setJavaScriptEnabled(true);
+        view.webView.setVisibility(View.INVISIBLE); // Hide the WebView initially
 
-        webView.setWebViewClient(new WebViewClient() {
+        view.webView.setWebViewClient(new WebViewClient() {
             @Override
-            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            public void onPageStarted(WebView webview, String url, Bitmap favicon) {
                 // Called when the WebView starts loading a new page
-                webView.setVisibility(View.INVISIBLE); // Hide the WebView
+                view.webView.setVisibility(View.INVISIBLE); // Hide the WebView
             }
 
             @Override
-            public void onPageFinished(WebView view, String url) {
+            public void onPageFinished(WebView webview, String url) {
                 // Called when the WebView finishes loading a page
-                view.loadUrl("javascript:(function() { " +
+                webview.loadUrl("javascript:(function() { " +
                         "var element = document.getElementsByClassName('max-w-8xl mx-auto p-4 md:px-6 flex flex-col')[0]; " +
                         "document.body.innerHTML = element.outerHTML; })();");
-                webView.setVisibility(View.VISIBLE); // Show the WebView after the page has finished loading
+                view.webView.setVisibility(View.VISIBLE); // Show the WebView after the page has finished loading
             }
         });
-        webView.loadUrl(url);
+        view.webView.loadUrl(url);
     }
 }
