eb3234c059c6bc516586b0ad17fba7eb58df9ad9
Neil Prabhu <nprabhu@kth.se>
2024-08-24 19:40:15 -0500

    Added viewbinding to VS Activity

--- app/src/main/java/com/example/pilotpal/VSActivity.java
+++ app/src/main/java/com/example/pilotpal/VSActivity.java
@@ -6,14 +6,21 @@ import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
+import com.example.pilotpal.databinding.ActivityDistanceBinding;
+import com.example.pilotpal.databinding.ActivityVSBinding;
+
+import java.util.Objects;
+
 public class VSActivity extends AppCompatActivity {
+    ActivityVSBinding view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        view = ActivityVSBinding.inflate(getLayoutInflater());
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        getSupportActionBar().hide();
-        setContentView(R.layout.activity_v_s);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+        setContentView(view.getRoot());
     }
 }
